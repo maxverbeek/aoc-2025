4dce17c775b907d0b52aaaa1ce1b8220e5cf4c0b
Max Verbeek <m4xv3rb33k@gmail.com>
2025-12-02 22:14:43 +0100

    feat: day 2

---
+++ app/Day02.hs
@@ -0,0 +1,43 @@
+module Main where
+
+import qualified Aoc
+
+parseLine :: String -> [(Int, Int)]
+parseLine line = map parsePair (Aoc.splitOn ',' line)
+  where
+    parsePair s = case Aoc.splitOn '-' s of
+      [a, b] -> (read a, read b)
+      _ -> error "invalid pair"
+
+main :: IO ()
+main = do
+  input <- getLine
+  let parsed = parseLine input
+  print (solve1 parsed)
+  print (solve2 parsed)
+
+
+ids :: (Int, Int) -> [String]
+ids (a, b) = [ show x | x <- [a..b] ]
+
+twiceRepeated id = even len && first == second
+  where
+  len = (length id)
+  halflen = len `div` 2
+  first = take halflen id
+  second = drop halflen id
+
+repeated needle haystack = all id $ zipWith (==) haystack (cycle needle)
+
+manyRepeated :: String -> Bool
+manyRepeated id = any (\x -> x) repeats
+  where
+  len = length id
+  repeats = [ (len `mod` c == 0) && repeated (take c id) (drop c id) | c <- [1..(len `div` 2)] ]
+  
+
+solve1 :: [(Int, Int)] -> Int
+solve1 = sum . map read . filter twiceRepeated . concatMap ids
+
+solve2 :: [(Int, Int)] -> Int
+solve2 = sum . map read . filter manyRepeated . concatMap ids
--- src/Aoc.hs
+++ src/Aoc.hs
@@ -1,4 +1,4 @@
-module Aoc (LetterDirection (..), intoLetterDirection, readLines) where
+module Aoc (LetterDirection (..), intoLetterDirection, readLines, splitOn) where
 
 data LetterDirection = L | R deriving Show
 
@@ -8,3 +8,8 @@ intoLetterDirection x = Nothing
 
 readLines :: IO [String]
 readLines = lines <$> getContents
+
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn delim xs = case break (== delim) xs of
+  (a, []) -> [a]
+  (a, _:b) -> a : splitOn delim b
