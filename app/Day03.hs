0ae6f943db9ff67a8a940073ed2d8eec325fb2a0
Max Verbeek <m4xv3rb33k@gmail.com>
2025-12-03 09:24:36 +0100

    style: some cleaning up

--- app/Day03.hs
+++ app/Day03.hs
@@ -1,32 +1,19 @@
 module Main where
 
-import qualified Aoc
-import Data.Char (digitToInt)
+import Aoc (readLines, after, droplast)
 
 main :: IO ()
 main = do
-  input <- Aoc.readLines
+  input <- readLines
   print (solve1 input)
   print (solve2 input)
-
-solve1 = sum . map (\(a,b) -> a * 10 + b) . map max2
-
-max2 chars = (first, second)
-  where
-  first = (maximum . toDigits . init) chars
-  lastbit = after first (toDigits chars)
-  second = maximum lastbit
-
-after x = tail . dropWhile (\c -> c /= x)
-
-droplast n = reverse . drop n . reverse
 
 maxn :: Int -> [Char] -> [Char]
 maxn 0 _ = []
 maxn n chars = let first = maximum (droplast (n-1) chars) in first : maxn (n - 1) (after first chars)
 
-toDigits :: String -> [Int]
-toDigits = map digitToInt
+solve1 :: [[Char]] -> Int
+solve1 = sum . map read . map (maxn 2)
 
 solve2 :: [[Char]] -> Int
 solve2 = sum . map read . map (maxn 12)
--- src/Aoc.hs
+++ src/Aoc.hs
@@ -1,4 +1,5 @@
-module Aoc (LetterDirection (..), intoLetterDirection, readLines, splitOn) where
+module Aoc where
+import Data.Char (digitToInt)
 
 data LetterDirection = L | R deriving Show
 
@@ -13,3 +14,12 @@ splitOn :: Eq a => a -> [a] -> [[a]]
 splitOn delim xs = case break (== delim) xs of
   (a, []) -> [a]
   (a, _:b) -> a : splitOn delim b
+
+after :: Eq a => a -> [a] -> [a]
+after x = tail . dropWhile (\c -> c /= x)
+
+droplast :: Int -> [a] -> [a]
+droplast n = reverse . drop n . reverse
+
+toDigits :: String -> [Int]
+toDigits = map digitToInt
