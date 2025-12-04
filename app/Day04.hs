bd336a54b0bc98d814e5801e571e0641bb716215
Max Verbeek <m4xv3rb33k@gmail.com>
2025-12-04 20:32:22 +0100

    refactor: make some more reusable stuff

--- app/Day04.hs
+++ app/Day04.hs
@@ -1,27 +1,19 @@
 module Main where
 
+import Aoc ((.+))
 import qualified Aoc
 import qualified Data.Set as S
 
 main :: IO ()
 main = do
-  content <- Aoc.readLines
-  let locs = locations content
+  content <- getContents
+  let locs = S.fromList $ Aoc.readAsCoords (== '@') content
   print $ length $ solve1 locs
   print $ solve2 0 locs
 
 type Location = (Int, Int)
 
-(.+) :: Location -> Location -> Location
-(.+) (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
-
-locations :: [String] -> S.Set Location
-locations lines = S.fromList [ (x, y) | (y, line) <- zip [0.. ] lines, (x, c) <- zip [0..] line, c == '@' ]
-
-directions :: [Location]
-directions = [(x, y) | x <- [-1, 0, 1], y <- [-1, 0, 1], (x, y) /= (0,0)]
-
-neighbours p locs = S.fromList [ p .+ d | d <- directions ] `S.intersection` locs
+neighbours p locs = S.fromList [ p .+ d | d <- Aoc.directions8 ] `S.intersection` locs
 
 solve1 :: S.Set Location -> S.Set Location
 solve1 locs = S.filter (\p -> length (neighbours p locs) < 4) locs
