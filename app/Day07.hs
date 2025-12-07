f329ef90a2a32a954436b4307ad77cbc6558607d
Max Verbeek <m4xv3rb33k@gmail.com>
2025-12-07 16:28:42 +0100

    style: cleanup

--- app/Day07.hs
+++ app/Day07.hs
@@ -1,5 +1,4 @@
 import qualified Data.Set as S
-import Debug.Trace
 
 main :: IO ()
 main = do
@@ -10,12 +9,14 @@ main = do
 
 solve1 :: [Bool] -> [String] -> Int
 solve1 beams [] = 0
-solve1 beams (line : rest) = (length splits) + solve1 newbeams rest
+solve1 beams (line : rest) = (length $ filter id split) + solve1 combined rest
   where
-  splits = [ [ i-1, i+1 ] | (i, beam, splitter) <- zip3 [0..] beams line, beam, splitter == '^' ]
-  splitbeams = S.filter (\l -> l >= 0 && l < length line) $ S.fromList $ concat splits
-  continues = S.fromList [ i | (i, air, beam) <- zip3 [0..] line beams, beam, air == '.' ]
-  newbeams = [ loc `elem` splitbeams || loc `elem` continues | (loc, _) <- zip [0..] line ]
+  continued  = [ c == '.' && beam | (beam, c) <- zip beams line ]
+  split      = [ c == '^' && beam | (beam, c) <- zip beams line ]
+  splitleft  = tail split ++ [False]
+  splitright = False : init split
+  combined   = zipWith3 (\a b c -> a || b || c) splitleft splitright continued
+
 
 solve2 :: [Integer] -> [String] -> [Integer]
 solve2 timelines [] = timelines
