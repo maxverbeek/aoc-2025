340fd36b0ccb7d9a775062af7e8e0070463f07f3
Max Verbeek <m4xv3rb33k@gmail.com>
2025-12-07 16:18:00 +0100

    feat: haskell is hard

--- app/Day07.hs
+++ app/Day07.hs
@@ -1,41 +1,28 @@
 import qualified Data.Set as S
-import Aoc ((.+), readAsCoords)
-
-type Loc = (Int, Int)
+import Debug.Trace
 
 main :: IO ()
 main = do
-  contents <- getContents
-  let oob = makeoob contents
-  let start = head $ readAsCoords (== 'S') contents
-  let splitters = readAsCoords (== '^') contents
-  print $ solve1 start S.empty (S.fromList splitters) oob
+  field <- lines <$> getContents
+  let beams = map (== 'S') (head field)
+  print $ solve1 beams (tail field)
+  print $ sum $ solve2 [ if b then 1 else 0 | b <- beams ] (tail field)
 
-makeoob :: String -> Loc -> Bool
-makeoob content = (\(x, y) -> x < 0 || x >= width || y < 0 || y >= height)
+solve1 :: [Bool] -> [String] -> Int
+solve1 beams [] = 0
+solve1 beams (line : rest) = (length splits) + solve1 newbeams rest
   where
-  l = lines content
-  width = length $ head l
-  height = length l
+  splits = [ [ i-1, i+1 ] | (i, beam, splitter) <- zip3 [0..] beams line, beam, splitter == '^' ]
+  splitbeams = S.filter (\l -> l >= 0 && l < length line) $ S.fromList $ concat splits
+  continues = S.fromList [ i | (i, air, beam) <- zip3 [0..] line beams, beam, air == '.' ]
+  newbeams = [ loc `elem` splitbeams || loc `elem` continues | (loc, _) <- zip [0..] line ]
 
-
-splitleft = (-1, 1)
-splitright = (1, 1)
-dontsplit = (0, 1)
-
-solve1 :: Loc -> S.Set Loc -> S.Set Loc -> (Loc -> Bool) -> Int
-solve1 (sx, sy) beams splitters oob = let (count, _) = go (sx, sy+1) beams in count
+solve2 :: [Integer] -> [String] -> [Integer]
+solve2 timelines [] = timelines
+solve2 timelines (line : rest) = solve2 combined rest
   where
-  go :: Loc -> S.Set Loc -> (Int, S.Set Loc)
-  go pos beams
-    | oob pos = (0, beams)
-    | pos `elem` beams = (0, beams)
-    | pos `elem` splitters = ((lc + rc), rb)
-    | otherwise = go pos' beams'
-    where
-    pos' = pos .+ dontsplit
-    pl = pos .+ splitleft
-    pr = pos .+ splitright
-    beams' = S.insert pos' beams
-    (lc, lb) = if pl `elem` beams || oob pl then (0, beams) else let (c, lb) = go (pos .+ splitleft) beams in (1+c, lb)
-    (rc, rb) = if pr `elem` beams || oob pr then (0, beams) else let (c, rb) = go (pos .+ splitright) lb in (1+c, rb)
+  continued  = [ if c == '.' then tl else 0 | (tl, c) <- zip timelines line ]
+  split      = [ if c == '^' then tl else 0 | (tl, c) <- zip timelines line ]
+  shiftleft  = tail split ++ [0]
+  shiftright = 0 : init split
+  combined   = zipWith3 (\a b c -> a + b + c) shiftleft shiftright continued
